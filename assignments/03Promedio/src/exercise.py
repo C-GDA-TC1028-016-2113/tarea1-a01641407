eb00ad36ff7b89a20136b91721ce06aea5faca90
a01641407 <A01641407@itesm.mx>
2021-08-19 19:20:41 +0000

    enter

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
+   
+    cal1 = int(input("Calificación de la materia: "))
+    cal2 = int(input("Calificación de la materia: "))
+    cal3 = int(input("Calificación de la materia: "))
+    cal4 = int(input("Calificación de la materia: "))
+    promedio = ((cal1 + cal2 + cal3 + cal4)/4) 
+    print("El promedio es:",promedio)
+        
 if __name__ == '__main__':
     main()
